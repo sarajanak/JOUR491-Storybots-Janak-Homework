34d526761751b2634d4104dca27ad62bd3c6b8d8
Sara Janak <sarajanak@Macintosh-e80688d7b4dc.local>
2014-09-09 15:37:51 -0500

    Adding the mad libs assignments that fill out sentences in python.

---
+++ madlibassignment.py
@@ -0,0 +1,8 @@
+noun = "Harper's Bazaar"
+verb = "covered"
+adjective = "flawless"
+proper_name = "Lady Gaga"
+quantity = 22
+
+
+print "%s %s the September issue of %s and she looked so %s I bought %s copies." % (proper_name, verb, noun, adjective, quantity)
